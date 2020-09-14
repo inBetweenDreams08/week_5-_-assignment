2e79b8ea6357281e6a91f0553b019ec2896c849f
Zack Werner <zackw0430@gmail.com>
2020-09-14 14:54:23 -0500

    Starting on hw.

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,1 +1,34 @@
 console.log('js');
+
+let vinyl_1 = {
+  title: "Ramble on",
+  artist: "Led Zeppelin",
+  year: 1969
+}; // end of vinyl
+
+let vinyl_2 ={
+  title: "black",
+  artist: "Pearl Jam",
+  year: 1991
+}; // end of vinyl
+
+let vinyl_3 = {
+  title: "Ticket to Ride",
+  artist: "The Beatles",
+  year: 1965
+}; // end of vinyl
+
+
+
+
+let vinyl_collection = [];
+
+
+function addToCollection(title, artist, year){
+
+
+} // end of func.
+
+
+
+console.log( addToCollection() )
