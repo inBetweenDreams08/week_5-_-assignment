84add0be2dd1f9f11cc541e8637d513480f663c4
Zack Werner <zackw0430@gmail.com>
2020-09-15 12:39:11 -0500

    Starting hw.

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -19,16 +19,39 @@ let vinyl_3 = {
 }; // end of vinyl
 
 
-
-
+// - create a variable "collection" that is an empty array
 let vinyl_collection = [];
-
-
+// - add a function to your script named `addToCollection(title, artist, year)`
 function addToCollection(title, artist, year){
+  console.log("inside of addToCollection func.", title);
+}
+
+addToCollection(vinyl_1);
+//
+// that, when called, creates a new record object and pushes it into the "collection" array
+// - this function should return the newly created record object
 
 
-} // end of func.
 
 
+// function addToCollection(title, artist, year){
+//   console.log("inside of addToCollection", title);
+  // vinyl_collection.push(vinyl_4 = {
+  //   title: "Night Moves",
+  //   artist: "Bob Seger & The Silver Bullet Band",
+  //   year: 1976
+  // });
 
-console.log( addToCollection() )
+ // end of func.
+
+// addToCollection(title: "Night Moves");
+
+// console.log(vinyl_1.title);
+
+
+// addToCollection(vinyl_1);
+
+
+// console.log(addToCollection);
+// console.log(vinyl_collection);
+// console.log(vinyl_1)
