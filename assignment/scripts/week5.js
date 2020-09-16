d8562538d17f2abcd08b22b6d0bff25b57834a71
Zack Werner <zackw0430@gmail.com>
2020-09-16 13:04:29 -0500

    updating hw.

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,22 +1,22 @@
 console.log('js');
 
-let vinyl_1 = {
-  title: "Ramble on",
-  artist: "Led Zeppelin",
-  year: 1969
-}; // end of vinyl
 
-let vinyl_2 ={
-  title: "black",
-  artist: "Pearl Jam",
-  year: 1991
-}; // end of vinyl
+// Below are example of songs I want to push. 
 
-let vinyl_3 = {
-  title: "Ticket to Ride",
-  artist: "The Beatles",
-  year: 1965
-}; // end of vinyl
+//   title: "Ramble on",
+//   artist: "Led Zeppelin",
+//   year: 1969
+
+
+
+//   title: "Ticket to Ride",
+//   artist: "The Beatles",
+//   year: 1965
+
+
+
+
+
 
 
 // - create a variable "collection" that is an empty array
@@ -24,33 +24,68 @@ let vinyl_collection = [];
 // - add a function to your script named `addToCollection(title, artist, year)`
 function addToCollection(title, artist, year){
   console.log("inside of addToCollection func." +" Title "+ title + " Artist "+ artist + " Year " + year);
-  vinyl_collection.push(title, artist, year);
+
+  // that, when called, creates a new record object and pushes it into the "collection" array
+  // - this function should return the newly created record object
+ let vinyl_record = {
+   title: title,
+   artist: artist,
+   year: year
+ };
+
+  vinyl_collection.push(vinyl_record);  // this is sending the vinyl_record into the array.
 }
-
-addToCollection(vinyl_4 = {
-  title: "Night Moves",
-  artist: "Bob Seger & The Silver Bullet Band",
-  year: 1976
-});
+// title, artist, year
+addToCollection( "Don't Stop", "Fleetwood Mac", 1988 ); // this is what I'm submitting into the title, artist, year.
 
 console.log(vinyl_collection);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("======================");
+// // stretch goals below
 //
-// that, when called, creates a new record object and pushes it into the "collection" array
-// - this function should return the newly created record object
-
-
-addToCollection(vinyl_5 ={});
-
-addToCollection
-(vinyl_5.title = "Don't Stop",
-vinyl_5.artist = "Fleetwood Mac",
-vinyl_5.year = 1988);
-
-console.log(vinyl_collection);
-
-
-
-
-// {
-//   title: "EverLong", artist: "FooFighters", year: 1997
+//
+// //
+// // - create the function `findRecord(title)`
+//
+// function findRecord(title){
+//   console.log(vinyl_collection.indexOf("Ramble on"));
+//   // if(vinyl_collection.indexOf("Ramble on") == -1 )
+//   // return false;
+//   // else {
+//   //   return true;
+//   // }
 // }
+//
+// console.log(findRecord());
+//
+//
+// // - this function should receive an argument of a record title
+// findRecord( vinyl_1.title = "Ramble on");
+// vinyl_5.title = "Dont Stop"
+// title: "EverLong", artist: "FooFighters", year: 1997
+
+// - if this record is found in the "collection" array,-
+// return the record's index within the array
+// - if the record is not found, return `false`
