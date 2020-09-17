ac737a7de1e78cf14c74a6564f23a17452a4fa54
Zack Werner <zackw0430@gmail.com>
2020-09-17 16:00:03 -0500

    updating hw.

--- assignment/scripts/week5.js
+++ assignment/scripts/week5.js
@@ -1,7 +1,7 @@
 console.log('js');
 
 
-// Below are example of songs I want to push. 
+// Below are example of songs I want to push/use.
 
 //   title: "Ramble on",
 //   artist: "Led Zeppelin",
@@ -23,69 +23,104 @@ console.log('js');
 let vinyl_collection = [];
 // - add a function to your script named `addToCollection(title, artist, year)`
 function addToCollection(title, artist, year){
-  console.log("inside of addToCollection func." +" Title "+ title + " Artist "+ artist + " Year " + year);
+  console.log("inside of addToCollection func." +" Title of the song "+ title + " Name of the Artist "+ artist + " The Year the song was released " + year);
 
   // that, when called, creates a new record object and pushes it into the "collection" array
   // - this function should return the newly created record object
- let vinyl_record = {
-   title: title,
-   artist: artist,
-   year: year
- };
+   let vinyl_record = {
+     title: title,
+     artist: artist,
+     year: year
+   };
 
   vinyl_collection.push(vinyl_record);  // this is sending the vinyl_record into the array.
 }
-// title, artist, year
-addToCollection( "Don't Stop", "Fleetwood Mac", 1988 ); // this is what I'm submitting into the title, artist, year.
+//    title, artist, year    // this is what I'm submitting into the title, artist, year.
+addToCollection( "Ticket to Ride", "The Beatles", 1965 );
+addToCollection("Ramble on","Led Zeppelin",1969);
+addToCollection("Eight Days a Week","The Beatles",1964);
 
-console.log(vinyl_collection);
-
-
-
-
+console.log(vinyl_collection);  // this is going to C.L. what's in the array.
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log("======================");
 // // stretch goals below
-//
-//
-// //
-// // - create the function `findRecord(title)`
-//
-// function findRecord(title){
-//   console.log(vinyl_collection.indexOf("Ramble on"));
-//   // if(vinyl_collection.indexOf("Ramble on") == -1 )
-//   // return false;
-//   // else {
-//   //   return true;
-//   // }
-// }
-//
-// console.log(findRecord());
-//
-//
-// // - this function should receive an argument of a record title
-// findRecord( vinyl_1.title = "Ramble on");
-// vinyl_5.title = "Dont Stop"
-// title: "EverLong", artist: "FooFighters", year: 1997
 
-// - if this record is found in the "collection" array,-
-// return the record's index within the array
+
+// - create the function `findRecord(title)`
+
+
+console.log('----------------------------------------');
+
+
+function findRecord(title){
+  console.log("inside function findRecord", title );
+  for(let i = 0; i < vinyl_collection.length; i++ ){  // this is looping through the array.
+    if(vinyl_collection[i].title === title ){   // this is finding the correct Title that is listed in the C.L. below.
+      return i ;  // this is going to tell me where the index spot is of the Title.
+    }
+  }
+    return false;
+}  // end of func.
+
+
+console.log(findRecord("Ramble on"));
+
+console.log(findRecord("Even Flow"));  // this is showing me the func. will return false!
+
+// - this function should receive an argument of a record title
+// - if this record is found in the "collection" array, return the record's index within the array
 // - if the record is not found, return `false`
+
+console.log("--------------------------------");
+// - create a function called `allBy(artist)`
+function allBy(artist){
+  console.log("inside function allBy ", artist);
+  // - when run, this function should return an array of all records in "collection"-
+  // - that are by the given artist
+  let resultArray = [];
+
+  for(let i = 0; i < vinyl_collection.length; i++ ){
+    if(vinyl_collection[i].artist === artist ){
+      resultArray.push(vinyl_collection[i]);
+    }
+  }
+    return resultArray;
+}
+
+console.log(allBy("The Beatles"));
+
+
+
+console.log("------------------------------");
+
+
+//
+    // - create a function called `search(criteria)`
+function search(criteria){
+
+
+    // - when run, this function should accept an object of search criteria,
+    // and return an array of matching records
+    //     - If there is no object `search()` or an empty object `search({})`, it should return all records
+
+  if(let = {}){
+   return vinyl_collection;
+  }
+    //     - If there is an artist property `search({artist: 'Ray Charles'})`, it should return all records by that artist
+    //     - If there is a year property `search({year: 1957})`, it should return all records from that year
+    //     - If there is a year property and an artist property `search({artist: 'Ray Charles', year: 1957})`,
+    //       it should return all records from that year by that artist
+}
+
+console.log(search({}))
+
+
+
+
+
+
+
+
+// ..
